5139386daa8c1c269762b0450b8d17cd2b12badd
Ted Leahy <tleahy@uplandsoftware.com>
2022-08-06 15:38:25 +0100

    Styles fixes

--- layouts/CenteredPage.tsx
+++ layouts/CenteredPage.tsx
@@ -10,23 +10,18 @@ const RootStyle = styled(Box)(() => ({
 	display: 'flex',
 	justifyContent: 'center',
 }));
-const PageContainer = styled(Box)(({ theme }) => ({
-	[theme.breakpoints.up('md')]: {
-		width: '50%',
-	},
-}));
 
 const CenteredPage = ({ children, title }: CenteredPageProps) => {
-    return (
+	return (
 		<RootStyle>
-			<PageContainer sx={{ mb: 5 }}>
+			<Box sx={{ mb: 5 }}>
 				<Typography variant="h3" sx={{ my: 5 }}>
 					{title}
 				</Typography>
-				{children}
-			</PageContainer>
+					{children}
+			</Box>
 		</RootStyle>
-    );
+	);
 };
 
 export default CenteredPage;
